237766beb78ad2f648685cf5b0bf9297bad5cf55
chensai <chensai123>
2019-06-24 15:20:58 +0800

    commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+>
+myleetcode.iml,1\e\1e4d61c5a294a9c26a1a2a9abf00a8233fc7fae5
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/java/com/test/One.java,2\5\2542f15e60dd947ce0a7425a7f8efc46af5f9f08
---
+++ src/main/java/com/test/TwoSum.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chensai
+ * @version 1.0
+ * @date 2019/6/21 17:57
+ *
+ * 两数之和
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] a = {2, 7, 11, 15};
+
+        System.out.println(Arrays.toString(sum3(a,22)));
+    }
+    public static int[] sum(int[] nums,int target){
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static int[] sum2(int[] a,int t){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<a.length;i++){
+            map.put(a[i],i);
+        }
+        for(int i=0;i<a.length;i++){
+            int x = a[i];
+            if(map.containsKey(t-x)&&map.get(t-x)!=i){
+                return new int[]{map.get(t-x),i};
+            }
+        }
+        return new int[0];
+
+    }
+    public static int[] sum3(int[] a,int t){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<a.length;i++){
+            int x = a[i];
+            int r = t-x;
+            if(map.containsKey(r)){
+                return new int[]{map.get(r),i};
+            }else {
+                map.put(x,i);
+            }
+
+        }
+        return new int[0];
+
+    }
+}
