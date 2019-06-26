baf6dfa263005e826523724bf732e0d4b9aee2b2
chensai <chensai123>
2019-06-26 16:07:32 +0800

    commit

--- src/main/java/com/test/algorithm/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/main/java/com/test/algorithm/LongestSubstringWithoutRepeatingCharacters3.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.test.algorithm;
 
 import java.util.HashMap;
 import java.util.HashSet;
--- src/main/java/com/test/algorithm/TwoSum1.java
+++ src/main/java/com/test/algorithm/TwoSum1.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.test.algorithm;
 
 import java.util.Arrays;
 import java.util.HashMap;
